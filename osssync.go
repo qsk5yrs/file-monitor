fa58043c09e7d681e12b0d445aa159bc70e31353
qsk5yrs <xuytwkqsd5m@gmail.com>
2024-10-26 16:17:33 +0800

    使用rclone执行oss同步第一版，tags/c0

--- osssync.go
+++ osssync.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"github.com/robfig/cron/v3"
+	"os/exec"
 	"sync"
 	"time"
-
-	"github.com/robfig/cron/v3"
 )
 
 var taskMutex sync.Mutex
@@ -21,7 +22,7 @@ func longRunningTask() {
 	defer taskMutex.Unlock()
 
 	startTime := time.Now()
-	log("Task started.")
+	log("Test Task started.")
 	defer func() {
 		if r := recover(); r != nil {
 			log("Task panicked at %v: %v", time.Now(), r)
@@ -29,19 +30,64 @@ func longRunningTask() {
 	}()
 
 	// 模拟任务执行
-	time.Sleep(15 * time.Second)
+	time.Sleep(2 * time.Second)
 
-	log("Task finished, took %v.", time.Since(startTime))
+	log("Test Task finished, took %v.", time.Since(startTime))
+}
+
+func executeRCloneTask(sourceBucket, destBucket string) {
+	taskMutex.Lock()
+	defer taskMutex.Unlock()
+
+	startTime := time.Now()
+	log("RClone Sync Task started.")
+	defer func() {
+		if r := recover(); r != nil {
+			log("RClone Sync Task panicked at: %v", r)
+		}
+	}()
+
+	// 定义 rClone 命令及其参数
+	cmd := exec.Command("/usr/local/bin/rclone", "sync", sourceBucket, destBucket, "-v", "--delete-excluded", "--transfers=8", "--checkers=16")
+
+	// 捕获命令的标准输出和标准错误
+	var out, errOut bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &errOut
+
+	// 执行命令
+	err := cmd.Run()
+
+	if err != nil {
+		log("RClone Sync Task failed: %v", err)
+		if errOut.Len() > 0 {
+			lines := errOut.String()
+			for _, line := range lines {
+				log("Command stderr: %s", line)
+			}
+		}
+	} else {
+		if out.Len() > 0 {
+			lines := out.String()
+			for _, line := range lines {
+				log("Command stdout: %s", line)
+			}
+		}
+		log("RClone Sync Task finished, took %v", time.Since(startTime))
+	}
 }
 
 func main() {
 	// 定义命令行参数
 	startCmd := flag.Bool("start", false, "Start the cron job")
+	sourceBucket := flag.String("source", "", "Source bucket name")
+	destBucket := flag.String("dest", "", "Destination bucket name")
+	interval := flag.Int("interval", 10, "Interval in seconds for running the task")
 	helpCmd := flag.Bool("h", false, "Show help message")
 
 	flag.Parse()
 
-	if *helpCmd || !*startCmd && flag.NArg() == 0 {
+	if *helpCmd || !*startCmd || *sourceBucket == "" || *destBucket == "" {
 		fmt.Println("Usage: program [options]")
 		fmt.Println("Options:")
 		flag.PrintDefaults()
@@ -51,9 +97,10 @@ func main() {
 	// 如果指定了 -start 参数，则启动定时任务
 	if *startCmd {
 		c := cron.New()
-		_, err := c.AddFunc("@every 10s", func() {
+		_, err := c.AddFunc(fmt.Sprintf("@every %ds", *interval), func() {
 			go func() {
-				longRunningTask() // 使用新的goroutine来执行任务
+				//longRunningTask() // 使用新的goroutine来执行任务
+				executeRCloneTask(*sourceBucket, *destBucket)
 			}()
 		})
 		if err != nil {
